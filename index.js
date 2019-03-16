f6d12816f096abd300d9a082cab06d039313572e
Sara Elsayed <sara.alsayed.moustafa@gmail.com>
2019-03-16 19:12:30 +0200

    add simple server

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function(req, res){
+    debugger;
+  res.send('Home');
+});
+app.get('/image', function(req, res){
+    var image = req.query.name;
+    res.sendFile(path.join(__dirname,`./assets/${image}.jpg`));
+    console.log(path);
+});
+app.listen(5000);
